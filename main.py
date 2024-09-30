14e661accb9610c91a90ee6ea38951a46569324a
YangTh599 <YangTh599@northwested.org>
2024-09-30 10:37:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,35 @@
 
 superheroes = {'Ironman', 'Spiderman', "Captain America", "Wolverine", "Deadpool","Hawkeye"}
 
-fav_superhero = 'Spiderman'
+fav_superhero = input('What is your favorite superhero?:\n')
 
-if fav_superhero in superheroes:
-    print('Your favorite superhero '+fav_superhero+' is in the list.')
+if fav_superhero.title() in superheroes:
+    print('Your favorite superhero '+fav_superhero.title()+' is in the list.')
 else:
-    print('Your favorite superhero '+fav_superhero+' is not in the list.')
+    print('Your favorite superhero '+fav_superhero.title()+' is not in the list.')
 
+print()
 
 # Task 2
 
-quiz_score = 92
+quiz_score = int(input('Enter your integer quiz score:\n'))
 
 if quiz_score >=85:
     print('You\'ve earn a B')
 else:
     print('You got lower than a B')
+print()
 
 # Task 3
 
-quote = 'May the force be with you.'
+quote = input('Enter a quote:\n')
 
 if quote.startswith('M'):
     print("Quote starts with \"M\".")
 else:
     print("Quote does not start with \"M\".")
+
+print()
 
 # Task 4
     
@@ -40,6 +44,8 @@ if filename.endswith('.png'):
     print('Filename does end in ".png".')
 else:
     print('Filename does not end in ".png".')
+
+print()
 
 # Task 5
     
